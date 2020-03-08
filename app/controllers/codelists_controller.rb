bd25a76a55f3e320f537d01d82aa4d9614f48281
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2020-03-08 16:26:50 +0900

    item delete

--- app/controllers/codelists_controller.rb
+++ app/controllers/codelists_controller.rb
@@ -1,4 +1,6 @@
 class CodelistsController < ApplicationController
+  before_action :correct_user,   only: :destroy
+
   def index
     @codelists = Codelist.paginate(page: params[:page])
   end
@@ -6,4 +8,17 @@ class CodelistsController < ApplicationController
   def show
     @codelist = Codelist.find(params[:id])
   end
+
+  def destroy
+    @codelist.destroy
+    flash[:success] = "アイテムを削除しました。"
+    redirect_to codelists_url
+  end
+
+  private
+
+    def correct_user
+      @codelist = current_user.codelists.find_by(id: params[:id])
+      redirect_to root_url if @codelist.nil?
+    end
 end
