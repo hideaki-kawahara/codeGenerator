29c88dcb7e0b1c22765d719b2de1c4c01b7a6f24
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2020-03-08 14:03:45 +0900

    update show

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,7 +2,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [365, 512]
+  process resize_to_limit: [368, 512]
 
   # Choose what kind of storage to use for this uploader:
   # storage :fog
