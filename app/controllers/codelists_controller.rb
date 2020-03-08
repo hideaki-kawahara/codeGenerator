621725b67e4cdb7418cda7e082bdfae8c0cbb76c
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2020-03-08 12:33:00 +0900

    update bootstrap design

--- app/controllers/codelists_controller.rb
+++ app/controllers/codelists_controller.rb
@@ -2,4 +2,8 @@ class CodelistsController < ApplicationController
   def index
     @codelists = Codelist.paginate(page: params[:page])
   end
+
+  def show
+    @codelist = Codelist.find(params[:id])
+  end
 end
